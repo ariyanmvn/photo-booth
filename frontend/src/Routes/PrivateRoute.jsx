1626862eef7cef15717cedf3c97bba81d8e1a3a8
ariyanmvn <ariyan@moveon.global>
2025-08-18 06:11:28 +0600

    feat:added private route

--- frontend/src/Routes/PrivateRoute.jsx
+++ frontend/src/Routes/PrivateRoute.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../Context/authcontext";
+import { Navigate, useLocation } from "react-router-dom";
+import LoadingSpinner from "../Components/LoadingSpinner";
 
-export default function PrivateRoute({children}) {
-  return (
-    <div>PrivateRoute</div>
-  )
+export default function PrivateRoute({ children }) {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+  if(user&&user?.email){
+    return children
+  }
+  return <Navigate state={location?.pathname} to={"/login"}/>;
 }
